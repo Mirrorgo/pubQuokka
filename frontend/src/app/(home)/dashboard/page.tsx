16d6a1871e12ffb3e7728a80c7fcf3479985baab
unimirrorgo <weizhaol2@student.unimelb.edu.au>
2023-09-14 00:30:36 +1000

    feat: pard of dashboard

---
+++ frontend/src/app/(home)/dashboard/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+import { Button, Card, Col, Row, Space, Typography } from "antd";
+import React, { useState } from "react";
+import styles from "./index.module.scss";
+import { type } from "os";
+type Notes = {
+  date: string;
+  type: string;
+};
+
+type DataSet = {
+  lastEditDate: string;
+  type: string;
+  owner: string;
+  size: number;
+};
+
+const { Title } = Typography;
+
+function DashBoard() {
+  const [unreadNotes, setUnreadNotes] = useState<Notes[]>([
+    {
+      date: "13/09/2023",
+      type: "Blood Oxygen Saturation",
+    },
+    {
+      date: "12/09/2023",
+      type: "Electrocardiography",
+    },
+  ]);
+  const [datasets, setDatasets] = useState<DataSet[]>([
+    {
+      lastEditDate: "13/09/2023",
+      type: "Blood Oxygen Saturation",
+      owner: "Jack",
+      size: 1000,
+    },
+    {
+      lastEditDate: "12/09/2023",
+      type: "Electrocardiography",
+      owner: "Tom",
+      size: 1500,
+    },
+  ]);
+  return (
+    <div>
+      <Title level={2}>DashBoard</Title>
+      <Space direction="vertical" className={styles["margin-content"]}>
+        <Row gutter={20}>
+          <Col span={6}>
+            <Title level={4}>Notes</Title>
+          </Col>
+          <Col span={15}>
+            <Title level={4}>You have {unreadNotes.length} unread notes</Title>
+          </Col>
+          <Col span={3}>
+            <Button type="primary">View All</Button>
+          </Col>
+        </Row>
+        <Card>
+          <Row gutter={20}>
+            {unreadNotes.map((cur, idx) => (
+              <Col key={idx}>
+                <Card className={styles.unreadNotes}>
+                  <div>New Dataset have been added</div>
+                  <div>TYPE: {cur.type}</div>
+                  <div>{cur.date}</div>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Card>
+        <Row gutter={20}>
+          <Col span={6}>
+            <Title level={4}>Datasets</Title>
+          </Col>
+          <Col span={15}>
+            <Title level={4}>You have {datasets.length} datasets</Title>
+          </Col>
+          <Col span={3}>
+            <Button type="primary">Hide</Button>
+          </Col>
+        </Row>
+        <Card>
+          <Space direction="vertical">
+            {datasets.map((cur, idx) => (
+              <Card key={idx} className={styles.dataSet}>
+                <div>TYPE: {cur.type}</div>
+                <div>Last edit: {cur.lastEditDate}</div>
+                <div>Dataset size: {cur.size}</div>
+                <div>Dataset owner: {cur.owner}</div>
+                <Row justify="space-evenly">
+                  <Col>
+                    <Button>Share the Data Set</Button>
+                  </Col>
+                  <Col>
+                    <Button>More details</Button>
+                  </Col>
+                </Row>
+              </Card>
+            ))}
+          </Space>
+        </Card>
+      </Space>
+    </div>
+  );
+}
+
+export default DashBoard;
--- frontend/src/app/(home)/layout.tsx
+++ frontend/src/app/(home)/layout.tsx
@@ -1,24 +1,35 @@
+"use client";
 import { Col, Divider, Image, Row, Space } from "antd";
 import styles from "./layout.module.scss";
+import { useRouter } from "next/navigation";
 
 export default function HomeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+
   return (
     <section>
       <Space style={{ width: "100vw" }} className={styles.headerFont}>
         <Image src="/image/validationBlack.png" alt="icon" width="30vw" />
         <Row justify="space-around" align="middle" style={{ width: "60vw" }}>
-          <Col className={styles["nav-item"]}>Dashboard</Col>
-          <Col className={styles["nav-item"]}>Generation</Col>
+          <Col className={styles["nav-item"]}>
+            <div onClick={() => router.push("dashboard")}>Dashboard</div>
+          </Col>
+          <Col
+            className={styles["nav-item"]}
+            onClick={() => router.push("generateForm")}
+          >
+            Generation
+          </Col>
           <Col className={styles["nav-item"]}>My DataSet</Col>
           <Col className={styles["nav-item"]}>Settings</Col>
         </Row>
       </Space>
       <Divider style={{ margin: "0" }} />
-      {children}
+      <div className={styles.container}>{children}</div>
     </section>
   );
 }
